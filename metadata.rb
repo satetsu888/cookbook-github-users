046c48c1b7c8728e1b7ab615ff4b75dc5484ce69
shotaro.akiyama <satetsu888@gmail.com>
2014-10-12 17:46:33 +0900

    ver 0.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'satetsu888@gmail.com'
 license          'All rights reserved'
 description      'Create users who has ssh-key on github and has dotfiles repo at his home dir'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 depends 'git'
 depends 'openssl'
